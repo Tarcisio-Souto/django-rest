937d4798ffc1be19756d0c0feffe0332ec668562
tarcisio_souto <tss.labsi@gmail.com>
2024-07-15 19:01:23 -0300

    atualizando models e migrations

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=250)
+    age = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
+
