9b16e1e1d75ed73c7eb242c0206fe4ed758d4266
tarcisio_souto <tss.labsi@gmail.com>
2024-07-17 18:32:54 -0300

    implementando o cadastro [POST]

--- app/views.py
+++ app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Store
+from .serializer import StoreSerializer
+
+@api_view(['GET', 'POST'])
+def list_stores(request):
+    if request.method == 'GET':
+        people = Store.objects.all()
+        serializer = StoreSerializer(people, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = StoreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
